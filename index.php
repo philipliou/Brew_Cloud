5e46228dbf6daed313f19e6f820fe57c6428450c
Steve Pappas <smpappas@gmail.com>
2012-11-16 15:56:40 -0500

    Theme updates

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 			<div class="row">
 				<div class="span12" style="text-align: center;">
 					<form action="search.php" method="GET">
-						<input class="search-box" type="text" name="query" placeholder="Search for beers or breweries" style="height: 28px; margin: 10px 0 16px 0; padding-left: 1%; padding-right: 1%;"></input>
+						<input class="search-box" type="text" name="query" placeholder="Search for beers or breweries" style="height: 36px; font-size: 16px; margin: 10px 0 16px 0; padding-left: 1%; padding-right: 1%;"></input>
 					</form>
 				</div>
 			</div>
